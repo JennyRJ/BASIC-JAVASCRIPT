b8addc050e563115e062147d74dd25e246f66338
JennyRJ <jeninepaula51@gmail.com>
2022-01-14 22:17:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("Hello World!");
+
+//Data type
+var a;
+a = 2;
+console.log(a);
+var myName = "Jenny";
+myName = "Lexie";
+console.log(myName);
+var b = "I am ";
+var correctCamelCase = "cute";
+var c;
+c = b + correctCamelCase;
+console.log(c);
+var sum = 4 + 4;
+/*var difference = 56 - 23;
+var product = 4 * 3;
+var divide = 45 / 9;
+console.log(sum);*/
+var aboutMe = 'I am good girl but i can also be a "bitch" when forced';
+console.log(aboutMe);
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
+var myFirstNamelength = 0;
+var myFirstName = "Jeniffer";
+myFirstNamelength = myFirstName.length;
+console.log(myFirstNamelength);
+var myArray = ["jenny", 45];
+myArray.push(["lexie", "Kevin"]);
+//myArray.pop();
+myArray.shift();
+myArray.unshift("baby");
+console.log(myArray);
+//functions
+
+function myFirst(a, b) {
+    console.log(a + b);
+}
+myFirst(3, 1);
+
+function() {
+    console.log();
+}
