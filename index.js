3409a1d1a5590cc4d70367d990b63681f57a5a83
JennyRJ <jeninepaula51@gmail.com>
2022-01-16 23:41:42 +0300

    More on functions

--- index.js
+++ index.js
@@ -56,3 +56,77 @@ var testArr = [1, 2, 3, 4, 5];
 console.log("Before" + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After" + JSON.stringify(testArr));
+
+function isItTrue() {
+    if (isItTrue) {
+        return "Yes,its true";
+    }
+    return "No,its not true";
+}
+console.log(isItTrue(true));
+
+function test(age) {
+    if (age == 20) {
+        return "You are a teenager";
+    }
+    return "You are an adult";
+}
+console.log(test(18));
+
+function strictEqualOperator(e, f) {
+    if (e === f) {
+        return "Equals";
+    }
+    return "Not equal";
+}
+console.log(strictEqualOperator(10, "10"));
+
+function notEqual(val) {
+    if (val != 10) {
+        return "Not equal";
+    }
+    return "Equal";
+}
+console.log(notEqual(4));
+
+function greaterThan(sum) {
+    if (sum > 10) {
+        return "over 10";
+    }
+    if (sum > 100) {
+        return "over 100";
+    }
+    return "Under both";
+}
+console.log(greaterThan(10));
+
+function greaterThanOrEqual(age) {
+    if (age >= 20) {
+        return "You are an adult";
+    }
+    return "You are a teenager";
+}
+console.log(greaterThanOrEqual(17));
+
+function lessThan(age) {
+    if (age < 15) {
+        return "You are a child";
+    }
+    if (age < 35) {
+        return "You are a youth";
+    }
+    return "Your category is not specified";
+}
+console.log(lessThan(50));
+
+function lessThanOrEqual(marks) {
+    if (marks <= 250) {
+        return "You are below average";
+    }
+    if (marks <= 100) {
+        return "You failed";
+    }
+    return "You passed";
+}
+
+console.log(lessThanOrEqual(300));
