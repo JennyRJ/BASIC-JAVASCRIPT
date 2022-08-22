ad67dee3226c4961fbbbc6c8afefe71d60e7e414
Jeniffer Chelangat <81527223+JennyRJ@users.noreply.github.com>
2022-08-22 18:55:11 +0300

    Add files via upload
    functions and objects

--- js-functions/src/script.js
+++ js-functions/src/script.js
@@ -10,7 +10,41 @@ function trippleNumber (x){
  //alert (myNumber)
  
  function aboutMe (name, age ,passion){
-   alert("My name is " +name+ ". I am " +age+ " years old.I am a " +passion+ " and i like it." )
+   //alert("My name is " +name+ ". I am " +age+ " years old.I am a " +passion+ " and i like it." )
  }
 aboutMe("Jeniffer","22" ,"Software developer")
+/*let cat = {
+        name: "bobby",
+        eyescolor: "blue",
+        age: 4,
+    }
+    cat.age
+   cat.name
+
+nested objects
+let fruits = {
+    favorite: "oranges",
+    least: "pawpaw",
+    most: "banana",
+    veges: {
+        like: "kales",
+        unlike: "cabbage",
+
+    },
+}*/
+//fruits.veges.unlike
+let dog = {
+  name:"bobby",
+  age:5,
+  eyeColor:"blue",
+  whoof(){
+    alert("whoooooof")
+  },
+}
+//dog.whoof()
+document.addEventListener("click", myFunction)
+
+function myFunction (){
+  alert("Hello there")
+}
 
