22273186fad7381f2686475f9414c8bbd94c8365
Jeniffer Chelangat <81527223+JennyRJ@users.noreply.github.com>
2022-08-22 12:35:43 +0300

    Add files via upload

---
+++ js-functions/README.md
@@ -0,0 +1,4 @@
+# JS functions
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jennyrj/pen/mdxoWMB](https://codepen.io/jennyrj/pen/mdxoWMB).
+
---
+++ js-functions/src/script.js
@@ -0,0 +1,16 @@
+function trial (myName){
+  alert ("hello my name is " + myName + ".")
+}
+//trial("Jenny")
+
+function trippleNumber (x){
+  return 3*x
+}
+ let myNumber = trippleNumber (12)
+ //alert (myNumber)
+ 
+ function aboutMe (name, age ,passion){
+   alert("My name is " +name+ ". I am " +age+ " years old.I am a " +passion+ " and i like it." )
+ }
+aboutMe("Jeniffer","22" ,"Software developer")
+
